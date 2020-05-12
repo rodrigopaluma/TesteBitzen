393b45f39c4398e241d4e3713e1af5331eea3333
rodrigopaluma <rodrigo.paluma@gmail.com>
2020-05-12 15:54:31 -0300

    [FEAT] Completion of the Employees page layout. Calling the employees API and writing the data received from the API.

--- src/app/tab2/tab2-routing.module.ts
+++ src/app/tab2/tab2-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab2Page } from './tab2.page';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
   {
@@ -10,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), HttpClientModule],
   exports: [RouterModule]
 })
 export class Tab2PageRoutingModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
+  empregados;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.empregados = this.http.get<any>('http://dummy.restapiexample.com/api/v1/employees').pipe(
+      map(empregados => {
+        return empregados.data;
+      })
+    )
+  }
 
 }
